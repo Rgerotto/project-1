1bc6b3ee3d7452ca9c0eaa2c37420bf60e5ecd30
Rafael Gerotto <rafael.gerotto@hotmail.com>
2023-10-24 00:08:15 +0200

    trying to upload the img to .front

--- js/index.js
+++ js/index.js
@@ -1,23 +1,93 @@
-const cards = [
+const grid = document.querySelector('#game-board');
+
+const characters = [
+    { name: 'aquaman', img: 'aquaman.jpg' },
+    { name: 'batman', img: 'batman.jpg' },
+    { name: 'captain america', img: 'captain-america.jpg' },
+    { name: 'fantastic four', img: 'fantastic-four.jpg' },
+    { name: 'flash', img: 'flash.jpg' },
+];
+/*CREATING NEW CARDS (CHARACTERS) */
+const createCard = () => {
+    const card = document.createElement('div'); // creating a div with class="card"
+    card.classname = 'card';
+
+    const front = document.createElement('div');//creating a div with class="face front"
+    front.className = 'face front';
+    front.style.backgroundImage() = `url${characters.img}`
+
+    const back = document.createElement('div');//creating a div with class="face back"
+    back.className = 'face back';
+
+    card.appendChild(front);
+    card.appendChild(back);
+
+    return card;
+    console.log(card);
+}
+createCard();
+
+
+
+const loadGame = () => {
+    characters.forEach((character) => {
+        const card = createCard();
+        grid.appendChild(card);
+    });
+};
+loadGame()
+
+
+
+
+
+/*
+const robbers = [
     {name: 'robbers', img: 'robbers.webp'},
     {name: 'robbers1', img: 'robbers1.webp'},
     {name: 'robbers2', img: 'robbers2.webp'},
     {name: 'robbers3', img: 'robbers3.webp'},
-    {name: 'robbers4', img: 'robbers4.avif'},
     {name: 'robbers5', img: 'robbers5.webp'},
-    {name: 'family', img: 'mother-and-father'},
-    {name: 'family', img: 'mother-and-father'},
-    {name: 'robbers', img: 'robbers.webp'},
-    {name: 'robbers1', img: 'robbers1.webp'},
-    {name: 'robbers2', img: 'robbers2.webp'},
-    {name: 'robbers3', img: 'robbers3.webp'},
-    {name: 'robbers4', img: 'robbers4.avif'},
-    {name: 'robbers5', img: 'robbers5.webp'},
-    {name: 'robbers3', img: 'robbers3.webp'},
-    {name: 'robbers4', img: 'robbers4.avif'},
-    {name: 'robbers5', img: 'robbers5.webp'},
-    {name: 'family', img: 'mother-and-father'},
-    {name: 'family', img: 'mother-and-father'},
-    {name: 'family', img: 'mother-and-father'}
 ]
-console.log(cards.length);
+const family = [
+    {name: 'family', img: 'mother-and-father'},
+    {name: 'family', img: 'mother-and-father'},
+]
+
+const homeAloneGame = new homeAlone(cards);
+window.addEventListener('load', (event) => {
+    let page = '';
+    homeAloneGame.cards.forEach((cardGame) => {
+        page += `<div class="card" data-card-name="${cardGame.name}">
+        <div class="back" name="${cardGame.img}"></div>
+        <div class="front" style="background: url(img/${cardGame.card}) no-repeat"></div>
+        </div>
+        `;
+    });
+    document.querySelector('#').innerHTML = page;
+
+    document.querySelectorAll('.card').forEach(('card') => {
+        card.addEventListener('click',() => {
+            
+        })
+    })
+});
+const grid = document.querySelector('.cards-container');
+
+/*SCORE 
+
+let score = 50;
+
+/*CARDS EMPTY 
+let firstCard = '';
+let secondCard = '';
+
+/*CHECKING CARDS 
+const checkCards = () =>{
+    if(firstCard === 'robbers'){
+        score-=10;
+        if(secondCard === 'family'){
+            score+=3;
+        }
+}
+*/
