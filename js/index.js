d5b850ceeffeaaee58f8f13f14a0940bf669665a
Rafael Gerotto <rafael.gerotto@hotmail.com>
2023-10-23 13:27:42 +0200

    upload before lunch

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const cards = [
+    {name: 'robbers', img: 'robbers.webp'},
+    {name: 'robbers1', img: 'robbers1.webp'},
+    {name: 'robbers2', img: 'robbers2.webp'},
+    {name: 'robbers3', img: 'robbers3.webp'},
+    {name: 'robbers4', img: 'robbers4.avif'},
+    {name: 'robbers5', img: 'robbers5.webp'},
+    {name: 'family', img: 'mother-and-father'},
+    {name: 'family', img: 'mother-and-father'},
+    {name: 'robbers', img: 'robbers.webp'},
+    {name: 'robbers1', img: 'robbers1.webp'},
+    {name: 'robbers2', img: 'robbers2.webp'},
+    {name: 'robbers3', img: 'robbers3.webp'},
+    {name: 'robbers4', img: 'robbers4.avif'},
+    {name: 'robbers5', img: 'robbers5.webp'},
+    {name: 'robbers3', img: 'robbers3.webp'},
+    {name: 'robbers4', img: 'robbers4.avif'},
+    {name: 'robbers5', img: 'robbers5.webp'},
+    {name: 'family', img: 'mother-and-father'},
+    {name: 'family', img: 'mother-and-father'},
+    {name: 'family', img: 'mother-and-father'}
+]
+console.log(cards.length);
