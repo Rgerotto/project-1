a85e32348314cbaf40bc7ba46d05bf81cecb5699
Rafael Gerotto <rafael.gerotto@hotmail.com>
2023-10-24 11:29:51 +0200

    click & flip done

--- js/index.js
+++ js/index.js
@@ -5,6 +5,10 @@ const characters = [
     { name: 'batman', img: 'batman.jpg' },
     { name: 'captain america', img: 'captain-america.jpg' },
     { name: 'fantastic four', img: 'fantastic-four.jpg' },
+    { name: 'aquaman', img: 'aquaman.jpg' },
+    { name: 'batman', img: 'batman.jpg' },
+    { name: 'captain america', img: 'captain-america.jpg' },
+    { name: 'fantastic four', img: 'fantastic-four.jpg' },
     { name: 'flash', img: 'flash.jpg' }
 ];
 
@@ -23,14 +27,17 @@ const createCard = (character) => {
     card.appendChild(front);
     card.appendChild(back);
     
-    document.querySelectorAll('.cards').forEach((eachCard) => {
+    /*CLICK SECTION & FLIP*/
+
+    document.querySelectorAll('.back').forEach((eachCard) => {
         eachCard.addEventListener('click',() => {
-        })
-    })
+            eachCard.classList.add('front')
+            eachCard.classList.remove('back')  
+        });
+    });
     return card;
-}
-
-
+};
+        
 
 
 const loadGame = () => {
@@ -48,6 +55,24 @@ loadGame()
 
 
 /*
+
+//SCORE 
+        let score = 50;
+            
+        //CARDS EMPTY 
+        //let firstCard = '';
+        //let secondCard = '';
+
+        //CHECKING CARDS 
+        const checkCards = () =>{
+            if(firstCard === 'robbers'){
+                score-=10;
+                if(secondCard === 'family'){
+                    score+=3;
+                };
+            };
+        });
+
     document.querySelectorAll('.card').forEach(('card') => {
         card.addEventListener('click',() => {
             
@@ -55,21 +80,4 @@ loadGame()
     })
 });
 const grid = document.querySelector('.cards-container');
-
-/*SCORE 
-
-let score = 50;
-
-/*CARDS EMPTY 
-let firstCard = '';
-let secondCard = '';
-
-/*CHECKING CARDS 
-const checkCards = () =>{
-    if(firstCard === 'robbers'){
-        score-=10;
-        if(secondCard === 'family'){
-            score+=3;
-        }
-}
 */
