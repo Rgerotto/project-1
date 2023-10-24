bca9d8013969cc0b2373ee6659824879628b722e
Rafael Gerotto <rafael.gerotto@hotmail.com>
2023-10-24 10:19:37 +0200

    back image working

--- js/index.js
+++ js/index.js
@@ -5,33 +5,38 @@ const characters = [
     { name: 'batman', img: 'batman.jpg' },
     { name: 'captain america', img: 'captain-america.jpg' },
     { name: 'fantastic four', img: 'fantastic-four.jpg' },
-    { name: 'flash', img: 'flash.jpg' },
+    { name: 'flash', img: 'flash.jpg' }
 ];
-/*CREATING NEW CARDS (CHARACTERS) */
-const createCard = () => {
+
+const createCard = (character) => {
+
     const card = document.createElement('div'); // creating a div with class="card"
-    card.classname = 'card';
+    card.classname = 'cards';
 
     const front = document.createElement('div');//creating a div with class="face front"
     front.className = 'face front';
-    front.style.backgroundImage() = `url${characters.img}`
+    front.style.backgroundImage = `url(/img/${character.img})`;
 
     const back = document.createElement('div');//creating a div with class="face back"
     back.className = 'face back';
 
     card.appendChild(front);
     card.appendChild(back);
-
+    
+    document.querySelectorAll('.cards').forEach((eachCard) => {
+        eachCard.addEventListener('click',() => {
+        })
+    })
     return card;
-    console.log(card);
 }
-createCard();
+
 
 
 
 const loadGame = () => {
+    console.log(characters.length);
     characters.forEach((character) => {
-        const card = createCard();
+        const card = createCard(character);
         grid.appendChild(card);
     });
 };
@@ -41,31 +46,8 @@ loadGame()
 
 
 
+
 /*
-const robbers = [
-    {name: 'robbers', img: 'robbers.webp'},
-    {name: 'robbers1', img: 'robbers1.webp'},
-    {name: 'robbers2', img: 'robbers2.webp'},
-    {name: 'robbers3', img: 'robbers3.webp'},
-    {name: 'robbers5', img: 'robbers5.webp'},
-]
-const family = [
-    {name: 'family', img: 'mother-and-father'},
-    {name: 'family', img: 'mother-and-father'},
-]
-
-const homeAloneGame = new homeAlone(cards);
-window.addEventListener('load', (event) => {
-    let page = '';
-    homeAloneGame.cards.forEach((cardGame) => {
-        page += `<div class="card" data-card-name="${cardGame.name}">
-        <div class="back" name="${cardGame.img}"></div>
-        <div class="front" style="background: url(img/${cardGame.card}) no-repeat"></div>
-        </div>
-        `;
-    });
-    document.querySelector('#').innerHTML = page;
-
     document.querySelectorAll('.card').forEach(('card') => {
         card.addEventListener('click',() => {
             
