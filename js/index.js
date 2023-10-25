4ebbfc5b0168470e75704a2b0da63355bcd10472
Rafael Gerotto <rafael.gerotto@hotmail.com>
2023-10-25 13:33:35 +0200

    Find a kevin done

--- js/index.js
+++ js/index.js
@@ -3,26 +3,35 @@ const points = document.querySelector('.result-of');
 const spanPlayer = document.querySelector('.player')
 let scoreNumber = 0;
 const characters = [
-    { name: 'robbers', img: 'robbers.webp', quantity: 5},
-    { name: 'robbers1', img: 'robbers1.webp', quantity: 5 },
-    { name: 'robbers2', img: 'robbers2.webp', quantity: 5 },
-    { name: 'robbers3', img: 'robbers3.webp', quantity: 5 },
-    { name: 'robbers4', img: 'robbers4.webp', quantity: 5 },
-    { name: 'robbers5', img: 'robbers5.webp', quantity: 5 },
-    { name: 'family', img: 'mother-and-father.jpeg', quantity: -5 },
-    /* { name: 'kevin', img: 'kevin.webp', quantity: -5 } */
+    { name: 'robbers', img: 'robbers.webp', quantity: 0},
+    { name: 'robbers1', img: 'robbers1.webp', quantity: 0 },
+    { name: 'robbers2', img: 'robbers2.webp', quantity: 0 },
+    { name: 'robbers3', img: 'robbers3.webp', quantity: 0 },
+    { name: 'robbers4', img: 'robbers4.webp', quantity: 0 },
+    { name: 'robbers5', img: 'robbers5.webp', quantity: 0 },
+    { name: 'family', img: 'mother-and-father.jpeg', quantity: 0 },
+    { name: 'robbers', img: 'robbers.webp', quantity: 0},
+    { name: 'robbers1', img: 'robbers1.webp', quantity: 0 },
+    { name: 'robbers2', img: 'robbers2.webp', quantity: 0 },
+    { name: 'robbers3', img: 'robbers3.webp', quantity: 0 },
+    { name: 'robbers4', img: 'robbers4.webp', quantity: 0 },
+    { name: 'robbers5', img: 'robbers5.webp', quantity: 0 },
+    { name: 'family', img: 'mother-and-father.jpeg', quantity: 0 },
+    { name: 'kevin', img: 'kevin.webp', quantity: 10 },
+
 ];
 
 
-const createCard = (character) => {
 
+const createCard = (character) => {
+    
     const card = document.createElement('div'); // creating a div with class="card"
     card.classname = 'cards';
-
+    
     const front = document.createElement('div');//creating a div with class="face front"
     front.className = 'face front';
     front.style.backgroundImage = `url(/img/${character.img})`;
-
+    
     const back = document.createElement('div');//creating a div with class="face back"
     back.className = 'face back';
     back.setAttribute("quantity", character.quantity)//set the attribute to find the quantity
@@ -35,37 +44,41 @@ const createCard = (character) => {
     return card;
 };
 
+/* const newObject = { name: 'kevin', img: 'kevin.webp', quantity: -5 };
+characters.push(newObject); */
+
 const loadGame = () => {
     //DUPLICATED AND SHUFFLE
-    const duplicatecharacters = [...characters];//duplicate the array
-    const shuffledArray = duplicatecharacters.sort(() => Math.random() - 0.9)//mix the cards on it game
+    const duplicatedArray = [...characters];//duplicate the array
+    const shuffledArray = duplicatedArray.sort(() => Math.random() - 0.5)//mix the cards on it game
     shuffledArray.forEach((character) => {
-        const card = createCard(character);
-        grid.appendChild(card);
-      });
-
-    characters.forEach((character) => {
         const card = createCard(character);
         grid.appendChild(card);
     });
 
+    /* characters.forEach((character) => {
+        const card = createCard(character);
+        grid.appendChild(card);
+    }); */
+    
     const timeOut = setTimeout(() => {
         document.querySelectorAll('.back').forEach((eachCard) => {
             eachCard.classList.remove('back')
             eachCard.classList.add('front')
         })
     },2000)
-
+    
     const TimeIn = setTimeout(() => {
         document.querySelectorAll('.front').forEach((eachCard) => {
             eachCard.classList.add('back')
             eachCard.classList.remove('front')
         })
     },5000)
-
     
-
-
+    
+    /* ADD UNDER HERE WAY OF TO DO NOT DISPLAY KEVIN DUPLICATED */
+    
+    
     //CLICK SECTION & FLIP
     document.querySelectorAll('.back').forEach((eachCard) => { //select class BACK
         eachCard.addEventListener('click',() => { //add click to work on it click with the mouse
@@ -86,8 +99,8 @@ const loadGame = () => {
     //SHOULD GO TO OTHER PAGE WITH KEVIN IMAGE SAYING
     // NO.....NO.... YOU LOSE!!
     const endGame = () => {
-        if (scoreNumber === 20) {
-            //return alert("Game Over!");
+        if (scoreNumber === 10) {
+            return alert("Game Over!");
         }
     };
 };
@@ -100,14 +113,6 @@ window.onload = () => {
 
 loadGame()
 
-//TO START GAME
-/*
-const TimeIn = setTimeout(timetoStart, 500)
-    function timeToStart(){
-        document.getElementsByTagName('body')
-    }
-
-*/
 
 
 
